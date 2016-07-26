c16dd30f11443973cb9e889d530bdda25660de22
Thetudors <152804004@ogr.cbu.edu.tr>
2016-07-26 03:47:09 +0300

    son ayar düzenlenmesi gerek
    son ayar düzenlenmesi gerek

--- blog/index.php
+++ blog/index.php
@@ -252,51 +252,23 @@ if(isset($_GET["query"]))
 							<section>
 							<h2>Güncel Konular</h2>
 								<ul class="posts">
+									<?php
+                                        $sqlc = "select * From blogyazilar,blogcomments ORDER BY blogcomments.Comment_Date DESC limit 0,5";
+										$queryc = mysqli_query($db,$sqlc);
+										 while( $rowc = mysqli_fetch_array( $queryc,MYSQLI_ASSOC ) ) {
+?>
 									<li>
 										<article>
 											<header>
-												<h3><a href="#">Lorem ipsum fermentum ut nisl vitae</a></h3>
-												<time class="published" datetime="2015-10-20">October 20, 2015</time>
-											</header>
-											<a href="#" class="image"><img src="images/pic08.jpg" alt="" /></a>
-										</article>
-									</li>
-									<li>
-										<article>
-											<header>
-												<h3><a href="#">Convallis maximus nisl mattis nunc id lorem</a></h3>
-												<time class="published" datetime="2015-10-15">October 15, 2015</time>
-											</header>
-											<a href="#" class="image"><img src="images/pic09.jpg" alt="" /></a>
-										</article>
-									</li>
-									<li>
-										<article>
-											<header>
-												<h3><a href="#">Euismod amet placerat vivamus porttitor</a></h3>
-												<time class="published" datetime="2015-10-10">October 10, 2015</time>
-											</header>
-											<a href="#" class="image"><img src="images/pic10.jpg" alt="" /></a>
-										</article>
-									</li>
-									<li>
-										<article>
-											<header>
-												<h3><a href="#">Magna enim accumsan tortor cursus ultricies</a></h3>
-												<time class="published" datetime="2015-10-08">October 8, 2015</time>
-											</header>
-											<a href="#" class="image"><img src="images/pic11.jpg" alt="" /></a>
-										</article>
-									</li>
-									<li>
-										<article>
-											<header>
-												<h3><a href="#">Congue ullam corper lorem ipsum dolor</a></h3>
-												<time class="published" datetime="2015-10-06">October 7, 2015</time>
+												<h3><a href="#"><?php echo $rowc["Article_Title"] ?></a></h3>
+												<time class="published" datetime="2015-10-06"><?php echo $rowc["Share_Date"] ?></time>
 											</header>
 											<a href="#" class="image"><img src="images/pic12.jpg" alt="" /></a>
 										</article>
 									</li>
+									<?php
+										 }
+									?>
 								</ul>
 							</section>
 
