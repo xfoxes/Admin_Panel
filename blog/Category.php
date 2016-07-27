ffdf7ec3763779d5dbb6ed52b6210b85c44b509d
Thetudors <152804004@ogr.cbu.edu.tr>
2016-07-27 22:34:09 +0300

    category

---
+++ blog/Category.php
@@ -0,0 +1,189 @@
+<?php 
+include "connect_mysql.php";
+$yaziid;
+session_start();
+
+if(isset($_GET["query"]))
+{
+	$sqlarama = "select * from blogyazilar WHERE blogyazilar.Article_Title LIKE '%".$_GET["query"]."%'";
+										$queryarama = mysqli_query($db,$sqlarama);
+										 while( $rowarama = mysqli_fetch_array( $queryarama,MYSQLI_ASSOC ) ) {
+											 $git = $rowarama["id"];
+										 }
+										 header ("location:http://localhost/Onat Aktas-me/Admin_Panel/blog/yazi.php?py=".$git);
+	
+}
+if(isset($_POST['cikisyap']))
+{
+	session_destroy();
+	header('Location: '.$_SERVER['REQUEST_URI']);
+}
+
+
+if(isset($_GET["py"]))
+{
+	$yaziid = $_GET["py"];
+}
+
+?>
+
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Single - Future Imperfect by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+	</head>
+	<body class="single">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<h1><a href="index.php">Onat Aktaş</a></h1>
+						<nav class="links">
+							<ul>
+								<li><a href="index.php">Anasayfa</a></li>
+								<li><a href="#">Hakkımda</a></li>
+								<li><a href="#">İletişim</a></li>
+								
+							</ul>
+						</nav>
+						<nav class="main">
+							<ul>
+								<li class="search">
+									<a class="fa-search" href="#search">Search</a>
+									<form id="search" method="get" action="#">
+										<input type="text" name="query" placeholder="Search" />
+									</form>
+								</li>
+								<li class="menu">
+									<a class="fa-bars" href="#menu">Menu</a>
+								</li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<section id="menu">
+
+						<!-- Search -->
+							<section>
+								<form class="search" method="get" action="#">
+									<input type="text" name="query" placeholder="Search" />
+								</form>
+								
+							</section>
+							<section>
+							<ul class="actions vertical">
+							<?php 
+							if(@$_SESSION['LoginCont'] != null)
+							{
+							?>
+                             <!-- GİRİŞ YAPILDIĞI DURUM -->
+							
+							<center><li> Merhaba <?php echo $_SESSION['KullaniciAdi']; ?> </li></center>
+							<li><a href="profil_settings.php" class="button big fit">Ayarlar</a></li><br>
+							<form method="post" action="#">
+							<li>
+							<button class="button big fit" name="cikisyap" type="submit" >Çıkış Yap </button>
+							</li>
+							</form>
+						    <?php
+							}
+							
+							else{
+								?>
+								<!-- GİRİŞ YAPILMADIĞI DURUM -->
+									<li><a href="login.php" class="button big fit">Giriş Yap</a></li>
+									<li><a href="register.php" class="button big fit">Kayıt Ol</a></li>
+									
+								
+							<?php
+							
+							}
+							?>
+									
+								</ul>
+							</section>
+
+						<!-- Links -->
+							<section>
+							<header>
+							<div class="title">
+							<center><h2>Kategoriler</h2></center>
+							<hr>
+							</div>
+							</header>
+							
+								<ul class="links">
+								<?php
+								$sqlcategory = "select * from blogcategory";
+										$querycategory = mysqli_query($db,$sqlcategory);
+										 while( $rowcategory = mysqli_fetch_array( $querycategory,MYSQLI_ASSOC ) ) {
+								?>
+									<li>
+										<a href="#">
+											<h3><?php echo $rowcategory["Category_Title"]?></h3>
+											<p><?php echo $rowcategory["Category_Desc"]?></p>
+										</a>
+									</li>
+									<?php
+										 }
+										 ?>
+								</ul>
+							</section>
+
+						<!-- Actions -->
+							
+
+
+					</section>
+
+				<!-- Main -->
+					<section>
+					
+					<article class="post">
+					<header>
+									<div class="title">
+										<h2><a href="#">Yazılım	</a></h2>
+									</div>		
+							</header>
+					        <header>
+									<div class="title">
+									<img height=100px src="../Admin/image/upload/bg.jpg" />
+										<h3>PHP dersleri</h3>
+										
+									</div>	
+
+							</header>
+				    </article>
+
+					</section>
+				<!-- Footer -->
+					<section id="footer">
+						<ul class="icons">
+							<li><a href="#" class="fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="#" class="fa-rss"><span class="label">RSS</span></a></li>
+							<li><a href="#" class="fa-envelope"><span class="label">Email</span></a></li>
+						</ul>
+						<p class="copyright">&copy; Untitled. Design: <a href="http://html5up.net">HTML5 UP</a>. Images: <a href="http://unsplash.com">Unsplash</a>.</p>
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
